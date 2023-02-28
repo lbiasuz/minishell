7bc702c8249064fa641a722b86b9a00e2dcd9dc7
remiranda <pesca_botoes.0t@icloud.com>
2023-02-28 11:11:18 -0300

    created file for pwd.c

---
+++ mini_builtins/pwd.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pwd.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmiranda <rmiranda@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/28 11:03:16 by rmiranda          #+#    #+#             */
+/*   Updated: 2023/02/28 11:10:42 by rmiranda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft/libft.h"
+#include <unistd.h> // getcwd getwd get_current_dir_name
+
+int	main(int argc, char *argv[])
+{
+	(void)argc;
+	(void)argv;
+	return (0);
+}
