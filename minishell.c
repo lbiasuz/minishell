07dc8f4e01550d1ff86fcfa4b1aa6576750708d3
remiranda <pesca_botoes.0t@icloud.com>
2023-03-13 12:44:58 -0300

    refinements to minishell.c

--- minishell.c
+++ minishell.c
@@ -6,25 +6,24 @@
 /*   By: rmiranda <rmiranda@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 10:30:16 by lbiasuz           #+#    #+#             */
-/*   Updated: 2023/03/12 19:11:21 by rmiranda         ###   ########.fr       */
+/*   Updated: 2023/03/13 12:41:33 by rmiranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int		execute_instruction(char **input);
 int		process_input(char *prompt);
-char	*wait_for_input(void);
-char	**parse_prompt(char	*prompt);
+char	**parse_and_free_prompt(char	*prompt);
+int		execute_and_free_instruction(char **input);
 
 int	main(int argc, char *argv[])
 {
-	int	prompt_limit_deguuber = 5;
+	int	prompt_limit_debugger = 5;
+	(void)argv;
 	if (argc >= 2)
 		return (-1);
-	while (!process_input(readline("MINI_PROMPT:")) && prompt_limit_deguuber--)
+	while (!process_input(readline("MINI_PROMPT:")) && prompt_limit_debugger--)
 		;
-	(void)argv;
 	return (0);
 }
 
@@ -34,25 +33,26 @@ int	process_input(char *prompt)
 
 	if (prompt == NULL)
 		return (-1);
-	input = parse_prompt(prompt);
-	free(prompt);
-	if (input)
+	input = parse_and_free_prompt(prompt);
+	if (!input)
 	{
-		execute_instruction(input);
-		free(input);
+		perror("error on ft_split");
+		exit(-1);
 	}
+	execute_and_free_instruction(input);
 	return (0);
 }
 
-char	**parse_prompt(char	*prompt)
+char	**parse_and_free_prompt(char	*prompt)
 {
 	char	**split_result;
 
 	split_result = ft_split(prompt, ' ');
+	free(prompt);
 	return (split_result);
 }
 
-int	execute_instruction(char **input)
+int	execute_and_free_instruction(char **input)
 {
 	int	i;
 
@@ -62,5 +62,7 @@ int	execute_instruction(char **input)
 		ft_printf("%s ", input[i]);
 		free(input[i++]);
 	}
+	ft_printf("\n", input[i]);
+	free(input);
 	return (0);
 }
