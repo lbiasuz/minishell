5d262ed56051b3e304d72b4a6e214cb3d50e8785
lbiasuz <biasuz.l.leonardo@gmail.com>
2023-03-25 23:34:16 -0300

    trying to make sense $ expansion substitution

--- include/token.h
+++ include/token.h
@@ -6,7 +6,7 @@
 /*   By: lbiasuz <lbiasuz@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 22:01:59 by lbiasuz           #+#    #+#             */
-/*   Updated: 2023/03/24 11:34:28 by lbiasuz          ###   ########.fr       */
+/*   Updated: 2023/03/25 21:24:19 by lbiasuz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,15 @@
 
 # define CHEV ">"
 # define ICHEV "<"
-# define DCHEV ">>"
-# define DICHEV "<<"
 # define SQUOTE "\'"
 # define DQUOTE "\""
 # define PIPE "|"
 # define SEMI ";"
 # define DOLL "$"
-# define AND "&&"
-# define OR "||"
-# define QUEST "?"
-# define CMD "COMMAND"
+# define ECOM "&"
 # define TEXT "TEXT"
 # define EXPAND "EXPAND"
 # define ERROR "Syntax error"
-# define HOME " ~ "
+# define HOME "~"
+
 #endif
--- src/token.c
+++ src/token.c
@@ -6,26 +6,76 @@
 /*   By: lbiasuz <lbiasuz@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 10:44:31 by lbiasuz           #+#    #+#             */
-/*   Updated: 2023/03/24 10:30:50 by lbiasuz          ###   ########.fr       */
+/*   Updated: 2023/03/25 23:32:50 by lbiasuz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "token.h"
 
+t_list	*plain_token(char *input)
+{
+	t_tkn	*token;
 
+	token = (t_tkn *)ft_calloc(1, sizeof(t_tkn));
+	if (!ft_strncmp(input, PIPE, sizeof(input)))
+		token->token = PIPE;
+	else if (!ft_strncmp(input, CHEV, sizeof(input)))
+		token->token = CHEV;
+	else if (!ft_strncmp(input, ICHEV, sizeof(input)))
+		token->token = ICHEV;
+	else if (!ft_strncmp(input, ECOM, sizeof(input)))
+		token->token = ECOM;
+	else if (!ft_strncmp(input, HOME, sizeof(input)))
+		token->token = EXPAND;
+	else
+		token->token = TEXT;
+	token->value = input;
+	return (ft_lstnew(token));
+}
+
+t_list	*compose_token(char *input)
+{
+	t_tkn	*token;
+	char	*addr;
+
+	token = (t_tkn *)ft_calloc(1, sizeof(t_tkn));
+	addr = ft_strchr(input, '$');
+	if (addr
+		&& ft_strchr(input, '\'') > addr 
+		&& char_occurences(input, '\'') == 2)
+		token->token = EXPAND;
+	token->value = TEXT;
+	return (ft_lstnew(token));
+}
 
 t_list	*tokenize(char *input)
 {
-	t_list	*token;
 	int		len;
-	
+
 	len = ft_strlen(input);
-	if (len == 1)
-		return (plain_token(input))
-	else if (len == 2 && !ft_isalnum(input[0]) && !ft_isalnum(input[1]))
-		return (plain_token(input))
-	
-	
-	return (token);
+	if (len == 1 && input[0] && !ft_isalnum(input[0]))
+		return (plain_token(input));
+	return (compose_token(input));
+}
+
+char	*scan_for_errors(t_list *token_list)
+{
+	...
+}
+
+int	char_occurences(char *string, int c)
+{
+	int	occurences;
+	int	index;
+
+	index = 0;
+	occurences = 0;
+	while (string[index])
+	{
+		if (string[index] == c)
+			occurences++;
+		index++;
+	}
+	return (occurences);
 }
