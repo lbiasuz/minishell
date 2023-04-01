5a866f4e44ae6ef9e1b5f5de08265efb185de814
remiranda <pesca_botoes.0t@icloud.com>
2023-04-01 00:59:00 -0300

    refined test makefile

--- test_minishell.mk
+++ test_minishell.mk
@@ -6,12 +6,13 @@
 #    By: rmiranda <rmiranda@student.42sp.org.br>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/03/31 21:15:56 by rmiranda          #+#    #+#              #
-#    Updated: 2023/04/01 00:47:04 by rmiranda         ###   ########.fr        #
+#    Updated: 2023/04/01 00:58:00 by rmiranda         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = test_output.txt
 
+TEST_INPUT = test_input.txt
 TEST_HASH = "e3ab27789415f3a2baf354b4f7299ac68df62f35 Utest_output.txt"
 TEST_SUBJECT = minishell
 
@@ -21,7 +22,7 @@ all: $(NAME)
 	@echo 'you might need this: echo "$(NAME)" >> .gitignore'
 
 $(NAME):
-	@./$(TEST_SUBJECT) < test_input.txt > $(NAME)
+	@./$(TEST_SUBJECT) < $(TEST_INPUT) > $(NAME)
 
 clean:
 	rm -f $(NAME)
