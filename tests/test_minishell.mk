902544e2beba3af9b95dbb143bbdb577b4ac517f
remiranda <pesca_botoes.0t@icloud.com>
2023-04-06 22:53:47 -0300

    fixed issue where test just showed the same result

--- tests/test_minishell.mk
+++ tests/test_minishell.mk
@@ -6,7 +6,7 @@
 #    By: rmiranda <rmiranda@student.42sp.org.br>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/03/31 21:15:56 by rmiranda          #+#    #+#              #
-#    Updated: 2023/04/06 10:20:21 by rmiranda         ###   ########.fr        #
+#    Updated: 2023/04/06 22:50:18 by rmiranda         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,7 +16,7 @@ TEST_INPUT = test_input.txt
 TEST_HASH = "44e3c15ad40d770f577a6c7b38666c8ab63d85d1 Utest_output.txt"
 TEST_SUBJECT = ../minishell
 
-all: $(NAME)
+all: clean $(NAME)
 	@echo $(TEST_HASH) | shasum -U -c
 	@echo "Test output file created, please run with the "clean" command. Ortherwise"
 	@echo 'you might need this: echo "$(NAME)" >> .gitignore'
