f3938d48e016cc222f52308975fea97f4961f2a5
remiranda <pesca_botoes.0t@icloud.com>
2023-05-09 11:00:52 -0300

    pull

--- src/syntax_analysis.c
+++ src/syntax_analysis.c
@@ -6,7 +6,7 @@
 /*   By: rmiranda <rmiranda@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 11:54:46 by rmiranda          #+#    #+#             */
-/*   Updated: 2023/05/09 10:59:49 by rmiranda         ###   ########.fr       */
+/*   Updated: 2023/05/09 11:00:23 by rmiranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,18 @@ static char	**syntax_error_location_or_null(char **parsed_input)
 	// int	chev_allowed;
 	// int	ecom_allowed;
 
-	pipe_allowed = 0;
-	chev_allowed = 0;
-	ecom_allowed = 0;
-	while (parsed_input)
-	{
-		if (pipe_allowed && ft_strncmp(parsed_input[0], DICHEV, sizeof(DICHEV)))
-			return (parsed_input);
-		if (chev_allowed && ft_strncmp(parsed_input[0], DICHEV, sizeof(DICHEV)))
-			return (parsed_input);
-		if (ecom_allowed && ft_strncmp(parsed_input[0], DICHEV, sizeof(DICHEV)))
-			return (parsed_input);
-	}
+	// pipe_allowed = 0;
+	// chev_allowed = 0;
+	// ecom_allowed = 0;
+	// while (parsed_input)
+	// {
+	// 	if (pipe_allowed && ft_strncmp(parsed_input[0], DICHEV, sizeof(DICHEV)))
+	// 		return (parsed_input);
+	// 	if (chev_allowed && ft_strncmp(parsed_input[0], DICHEV, sizeof(DICHEV)))
+	// 		return (parsed_input);
+	// 	if (ecom_allowed && ft_strncmp(parsed_input[0], DICHEV, sizeof(DICHEV)))
+	// 		return (parsed_input);
+	// }
 	return (NULL);
 }
 
