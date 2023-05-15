45632908a522d124db09f4942e4d6d5cd1de9a84
remiranda <pesca_botoes.0t@icloud.com>
2023-05-15 09:56:19 -0300

    Separated include files

---
+++ include/defines.h
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   defines.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmiranda <rmiranda@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/15 09:37:40 by rmiranda          #+#    #+#             */
+/*   Updated: 2023/05/15 09:42:50 by rmiranda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DEFINES_H
+# define DEFINES_H
+
+# define CHEV ">"
+# define DCHEV ">>"
+# define ICHEV "<"
+# define DICHEV "<<"
+# define SQUOTE "\'"
+# define DQUOTE "\""
+# define PIPE "|"
+# define SEMI ";" // not to be handled
+# define DOLL "$"
+# define ECOM "&"
+# define TEXT "TEXT"
+# define EXPAND "EXPAND"
+# define ERROR "Syntax error"
+# define HOME "~"
+# define NL "newline"
+# define PROMPT_DISPLAY_TEXT "MINI_PROMPT: "
+
+typedef struct s_ms {
+	char	**envp;
+	int		exit_code;
+}	t_ms;
+
+typedef struct s_cmd {
+	char	*exe;
+	char	*exe_path;
+	int		fd[2];
+	char	**args;
+	char	**raw;
+}	t_cmd;
+
+#endif
--- include/includes.h
+++ include/includes.h
@@ -1,22 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   init.c                                             :+:      :+:    :+:   */
+/*   includes.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rmiranda <rmiranda@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/08 09:57:05 by lbiasuz           #+#    #+#             */
-/*   Updated: 2023/05/15 09:53:55 by rmiranda         ###   ########.fr       */
+/*   Created: 2023/05/15 09:37:40 by rmiranda          #+#    #+#             */
+/*   Updated: 2023/05/15 09:44:47 by rmiranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <minishell.h>
+#ifndef INCLUDES_H
+# define INCLUDES_H
 
-t_ms	init_minishell(char **env)
-{
-	t_ms	ms;
+# include <libft.h>
+# include <ft_printf.h>
+// READLINE FUNCTION HEADERS
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <signal.h>
+# include <errno.h>
+// FILE HEADER
+# include <fcntl.h>
+# include <unistd.h>
+// PROCESS HEADERS
+# include <sys/wait.h>
 
-	ms.envp = copy_environment(env);
-	ms.exit_code = 0;
-	return (ms);
-}
+#endif
